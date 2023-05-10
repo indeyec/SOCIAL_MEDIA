fefe80f0ba2dbd430134035b0a9797801b7af5c3
qqqwwwbbb <usecaut@gmail.com>
2023-05-10 21:20:25 +0700

    Refactor usability tests.
    Co-authored-by: NAME <vlad.shilov03@mail.ru>
    Co-authored-by: ANOTHER-NAME <usecaut@gmail.com>

---
+++ appname/resources/js/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Hello, our Vue.js + Laravel project </div>
+
+                    <div class="card-body">
+                        Social messanger
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    setup() {
+
+    }
+}
+</script>
